fdcab92ffdcd05af97c9fd841bc6e4e05038dada
Josh <programmmerjosh@gmail.com>
2021-11-12 18:32:29 +0000

    first actual commit
    Home page and about page setup so far.

---
+++ blk-design-system-master/assets/js/footer-template.js
@@ -0,0 +1,197 @@
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = 
+        `
+        <!-- Form Modal -->
+        <div
+          class="modal fade modal-black"
+          id="myModal2"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="myModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header justify-content-center">
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-hidden="true"
+                >
+                  <i class="tim-icons icon-simple-remove text-white"></i>
+                </button>
+                <div class="text-muted text-center ml-auto mr-auto">
+                  <h3 class="mb-0">Contact</h3>
+                </div>
+              </div>
+              <div class="modal-body">
+                <div class="btn-wrapper text-center">
+                  <!-- could put something here -->
+                </div>
+                <div class="text-center text-muted mb-4 mt-3">
+                  <small>Feel free to ask any questions about my services.</small>
+                </div>
+                <form class="the-modal-form" role="form">
+                  <div class="form-group mb-3">
+                    <div class="input-group input-group-alternative">
+                      <div class="my-input-item">
+  
+                        <span class="input-group-text pr-3">
+                          Name
+                        </span>
+  
+                      </div>
+  
+                      <input
+                        class="form-control"
+                        placeholder="Name"
+                        type="name"
+                      />
+  
+                    </div>
+                  </div>
+  
+                  <div class="form-group mb-3">
+                    <div class="input-group input-group-alternative">
+                      <div class="">
+  
+                        <span class="input-group-text pr-3">
+                          Email
+                        </span>
+  
+                      </div>
+  
+                      <input
+                        class="form-control"
+                        placeholder="Email address"
+                        type="email"
+                      />
+  
+                    </div>
+                  </div>
+  
+                  <div class="form-group mb-3">
+                    <div class="input-group input-group-alternative">
+                      <div class="">
+  
+                        <span class="input-group-text pr-3 my-aqua-color">
+                          Message
+                        </span>
+  
+                      </div>
+  
+                      <textarea class="form-control" rows="5" cols="60" name="message" placeholder="Message"></textarea><br>
+  
+                    </div>
+                  </div>
+                  <div class="form-check mt-3">
+                    
+                  </div>
+                  <div class="text-center">
+                    <button type="button" class="btn btn-primary my-4">
+                      Send
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--  End Modal -->
+
+    </div>
+    <!--  End Of Main Section -->
+
+    <!-- JOSH USING -->
+    <!-- FOOTER SECTION => DONE -->
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <!-- <div class="col-md-3">
+            <h1 class="title">Services•</h1>
+          </div> -->
+          <div class="col-md-3">
+            <ul class="nav">
+              <li id="scroll-navigate" class="nav-item">
+                <a href="#page-top" class="nav-link"> Home </a>
+              </li>
+              <li class="nav-item">
+                <a href="pricing" class="nav-link">
+                  Pricing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="collaborate" class="nav-link">
+                  Collaborate
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="resume" class="nav-link">
+                  ResumÉ
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul class="nav">
+              <li class="nav-item">
+                <a data-toggle="modal" data-target="#myModal2" href="" class="nav-link">
+                  Contact
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="about" class="nav-link">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="blog" class="nav-link">
+                  Blog
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="disclaimer" class="nav-link">
+                  Disclaimer
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <h3 class="title">Connect with me:</h3>
+            <div class="btn-wrapper profile">
+              <a
+                target="_blank"
+                href="https://github.com/programmmerjosh"
+                class="btn btn-icon btn-neutral btn-round btn-simple"
+                data-toggle="tooltip"
+                data-original-title="GitHub"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <a
+                target="_blank"
+                href="https://www.instagram.com/josh_kenneth101/"
+                class="btn btn-icon btn-neutral btn-round btn-simple"
+                data-toggle="tooltip"
+                data-original-title="Instagram"
+              >
+                <i class="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- </div> -->
+
+`
+    }
+}
+
+customElements.define('my-footer', MyFooter);
+
+
+
---
+++ blk-design-system-master/assets/js/header-template.js
@@ -0,0 +1,145 @@
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = 
+        `
+        <!-- JOSH USING -->
+        <!-- Navbar => DONE -->
+        <nav
+          class="navbar navbar-expand-lg fixed-top navbar-transparent bg-neutral"
+          color-on-scroll="100"
+        >
+          <div class="container">
+            <div id="scroll-navigate" class="navbar-translate">
+              <a
+                class="navbar-brand"
+                href="#page-top"
+                rel="tooltip"
+                title="Typical stack: Bootstrap, HTML, CSS & jQuery"
+                data-placement="bottom"
+              >
+                <div
+                  id="joshua-van-niekerk"
+                  class="d-block d-sm-inline font-weight-bold"
+                  color-on-scroll="100"
+                >
+                  Joshua van Niekerk•
+                </div>
+                <div
+                  id="web-dev-freelancer"
+                  class="d-inline text-wrap"
+                  color-on-scroll="100"
+                >
+                  Web Developer Freelancer
+                </div>
+              </a>
+              <button
+                class="navbar-toggler navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navigation"
+                aria-controls="navigation-index"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-bar bar1" color-on-scroll="100"></span>
+                <span class="navbar-toggler-bar bar2" color-on-scroll="100"></span>
+                <span class="navbar-toggler-bar bar3" color-on-scroll="100"></span>
+              </button>
+            </div>
+            <div
+              class="collapse navbar-collapse justify-content-end"
+              id="navigation"
+            >
+              <div class="navbar-collapse-header">
+                <div class="row">
+                  <div class="col-6 collapse-brand">
+                    <a id="navbar-collapse-heading" color-on-scroll="100"> Freelancer </a>
+                  </div>
+                  <div class="col-6 collapse-close text-right">
+                    <button
+                      type="button"
+                      class="navbar-toggler"
+                      data-toggle="collapse"
+                      data-target="#navigation"
+                      aria-controls="navigation-index"
+                      aria-expanded="false"
+                      aria-label="Toggle navigation"
+                    >
+                      <i id="navbar-collapse-heading" class="tim-icons icon-simple-remove" color-on-scroll="100"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <ul class="navbar-nav">
+                <li class="nav-item p-0">
+                  <a
+                    class="nav-link"
+                    rel="tooltip"
+                    title="View some of my projects"
+                    data-placement="bottom"
+                    href="https://github.com/programmmerjosh" 
+                    target="_blank"
+                  >
+                    <i id="nav-link-social-media-icon" class="fab fa-github" color-on-scroll="100"></i>
+                    <p id="nav-link-social-media-text" class="d-lg-none d-xl-none" color-on-scroll="100">GitHub</p>
+                  </a>
+                </li>
+                <li class="nav-item p-0">
+                  <a
+                    class="nav-link"
+                    rel="tooltip"
+                    title="Follow me on Instagram"
+                    data-placement="bottom"
+                    href="https://www.instagram.com/josh_kenneth101/" target="_blank"
+                  >
+                    <i id="nav-link-social-media-icon" class="fab fa-instagram" color-on-scroll="100"></i>
+                    <p id="nav-link-social-media-text" class="d-lg-none d-xl-none" color-on-scroll="100">Instagram</p>
+                  </a>
+                </li>
+                <li class="dropdown nav-item">
+                  <a
+                    id="nav-link-getting-started"
+                    href="#"
+                    class="dropdown-toggle nav-link"
+                    data-toggle="dropdown"
+                    color-on-scroll="100"
+                  >
+                    <i class="fa fa-cogs d-lg-none d-xl-none"></i> Getting started
+                  </a>
+                  <div class="dropdown-menu dropdown-with-icons">
+                    <a href="pricing" class="dropdown-item">
+                      <i class="tim-icons icon-paper"></i>Pricing
+                    </a>
+                    <a href="collaborate" class="dropdown-item">
+                      <i class="tim-icons icon-bullet-list-67"></i>Collaborate
+                    </a>
+                    <a href="motivation" class="dropdown-item">
+                      <i class="tim-icons icon-image-02"></i>Motivation
+                    </a>
+                    <a href="about" class="dropdown-item">
+                      <i class="tim-icons icon-single-02"></i>About
+                    </a>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link btn btn-default d-none d-lg-block"
+                    rel="tooltip"
+                    title="I'm is also the creator of Goal Symmetry and Founder of Sagacious Apps"
+                    data-placement="bottom"
+                    href="https://www.sagaciousapps.com/apps" target="_blank"
+                  >
+                    <i class="tim-icons icon-mobile"></i> Goal Symmetry
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <!-- End Navbar -->
+
+`
+    }
+}
+
+customElements.define('my-header', MyHeader);
